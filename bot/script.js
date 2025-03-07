44791e69b7fb75f1c37eea66a4dd4d41b66464a8
Trebias <unitsss11@gmail.com>
2025-03-08 00:38:08 +0700

    bot

---
+++ bot/script.js
@@ -0,0 +1,23 @@
+
+
+var areas = document.getElementsByTagName("textarea");
+
+var itemList = JSON.parse(localStorage.getItem("qList"));
+if (itemList === null) {
+    itemList = {};
+}
+
+for (var i=0; i < areas.length; i++) {
+    areas[i].addEventListener("input", change);
+    if (i in itemList) {
+        areas[i].value = itemList[i];
+    }
+}
+console.log(itemList);
+
+function change() {
+    for (var i=0; i < areas.length; i++) {
+        itemList[i] = areas[i].value;
+    }
+    localStorage.setItem("qList", JSON.stringify(itemList));
+}
